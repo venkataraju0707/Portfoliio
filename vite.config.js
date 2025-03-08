a839aa0ad0e4d3dd864583fd26c3d5707d44806e
VENKATARAJU <161044578+venkataraju0707@users.noreply.github.com>
2025-03-08 20:36:10 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
-  },
+  base: '/Portfoliio/', // 👈 Add this (Replace "Portfoliio" with your repo name)
 });
